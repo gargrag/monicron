f56d38102fcc0f9d0be4e7d9ca1fe380e48cd8ad
Gastón Acosta <correo@gaston.com.uy>
2014-07-31 13:05:26 -0300

    first frontend approach

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    cronjobs = require('./routes/cronjobs');
+    cronjobs = require('./routes/cronjobs'),
+		frontend = require('./routes/frontend');
  
 var app = express();
 
@@ -9,13 +10,20 @@ app.get('/cronjobs/:server/:job_id', cronjobs.getPerId);
 
 app.put('/start/:server/:job_id/:frequency', cronjobs.startJob);
 app.put('/end/:server/:job_id', cronjobs.endJob);
+
+app.get('/', frontend.index);
+
  
 
 app.configure(function () {
     app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser());
+		app.set('view options', {layout : false});
+		app.set('view engine', 'jade');
+		app.set('views', __dirname + '/views');
+  	app.use(require('stylus').middleware(__dirname + '/public'));
+		app.use(express.static(__dirname + '/public'));
 });
  
 app.listen(3000);
 console.log('Listening on port 3000...');
-
